70d16668bfcce36194ec5c1e53630615891566f0
Juan Aristizabal-Zuluaga <jeaz.git@gmail.com>
2020-11-22 18:56:21 -0500

    feat: added tests for /cmodel/seirv_model.py

---
+++ tests/test_seirv_model.py
@@ -0,0 +1,128 @@
+from cmodel.seirv_model import Parameter, Variable
+from typing import List
+
+import pytest
+from pytest import approx
+
+from cmodel import seirv_model as seirv
+
+@pytest.fixture
+def state_variables_source():
+    state_variables_source = [
+        ("suceptible", "S", 50372424),
+        ("exposed", "Ex", 0),
+        ("infected", "If", 1),
+        ("recovered", "R", 0),
+        ("vaccinated", "V", 0),
+    ]
+
+    return state_variables_source
+
+@pytest.fixture
+def state_variables(state_variables_source):
+    state_variables = [
+        seirv.StateVariable(*sv) for sv in state_variables_source
+    ]
+
+    return state_variables
+
+
+@pytest.fixture
+def parameters_source():
+    parameters_source = [
+        ("Lambda", "Lambda", 2083.62),
+        ("mu", "mu", 0.0000150767),
+        ("alpha", "alpha", 0.172414),
+        ("omega", "omega", 0.0114),
+        ("gamma", "gamma", 0.0666667),
+        ("xi1", "xi1", 1.09736),
+        ("xi2", "xi2", 6.32609),
+        ("sigma", "sigma", 1.),
+        ("b1", "b1", 2.91823E-9),
+        ("b2", "b2", 2.67472E-9),
+        ("b3", "b3", 4.635E-9),
+        ("c1", "c1", 0.0000855641),
+        ("c2", "c2", 2.94906E-7),
+        ("c3", "c3", 0.159107),
+    ]
+    return parameters_source
+
+
+@pytest.fixture
+def parameters(parameters_source):
+    """The values of the parameters were obtained from Boris' optimization
+    script.
+    """
+    parameters = [seirv.Parameter(*param) for param in parameters_source]
+
+    return parameters
+
+
+def values_of_variables(variables: List[seirv.Variable]):
+    """Wraps all initial values of a list of Variable instances in a list"""
+    values = [var.initial_value for var in variables]
+    return values
+
+
+@pytest.fixture
+def model_SEIRV(state_variables, parameters):
+    t_span = [0, 171]
+    t_steps = 172
+
+    model_SEIRV = seirv.ModelSEIRV(state_variables, parameters, t_span, t_steps)
+    
+    return model_SEIRV
+
+
+def test_state_variables_initialization(
+    state_variables: List[seirv.StateVariable], state_variables_source
+):
+    state_variables_source_test = [
+        (sv.name, sv.representation, sv.initial_value) for sv in state_variables
+    ]
+
+    assert state_variables_source_test == state_variables_source
+
+
+def test_parameters_initialization(
+    parameters: List[seirv.Parameter], parameters_source
+):
+    parameters_source_test = [
+        (param.name, param.representation, param.initial_value) for param in parameters
+    ]
+
+    assert parameters_source_test ==  parameters_source
+
+
+def test_model_SEIRV_build_model(
+    model_SEIRV: seirv.ModelSEIRV,
+    state_variables: List[seirv.StateVariable],
+    parameters: List[seirv.Parameter]
+):
+    """Tests if evaluation of differential equation defined in SEIRV model is OK"""
+
+    state_variables = values_of_variables(state_variables)
+    parameters = values_of_variables(parameters)
+
+    # Expected value calculated in Mathematica, in Boris' script
+    expected_diff_eqn_value = approx(
+        [1324.04, 0.134732, -0.0780818, 0.0666667, 6.32609], rel=0.99
+    ) 
+
+    # Evaluation of differential equation must be independent of time
+    for t in range(10):
+        diff_eqn_value = model_SEIRV.build_model(
+            t, state_variables, *parameters
+        )
+        assert diff_eqn_value == expected_diff_eqn_value
+
+
+def test_model_SEIRV_run_model_initial_value(
+    model_SEIRV: seirv.ModelSEIRV,
+    state_variables: List[seirv.StateVariable]
+):
+    initial_state_variables = values_of_variables(state_variables)
+    solution = model_SEIRV.run_model()
+    solution_initial_values = [y[0] for y in solution.y]
+
+    assert solution_initial_values == approx(initial_state_variables)
