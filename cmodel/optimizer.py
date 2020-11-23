48aefd3b05691f3c1f53933de16a7da385f5460d
Juan Aristizabal-Zuluaga <jeaz.git@gmail.com>
2020-11-23 13:20:16 -0500

    feat: started working on optimizer

---
+++ cmodel/optimizer.py
@@ -0,0 +1,12 @@
+
+import cmodel.seirv_model as seirv
+
+class Optimizer:
+    def __init__(self, model) -> None:
+        pass
+
+    def cost_function(self):
+        """Function to be minimized by the optimizer
+        """
+
+        pass
