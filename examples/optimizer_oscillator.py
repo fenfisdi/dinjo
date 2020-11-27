3bbb7d7ef7075849411c4a3973abc4726a661426
Juan Aristizabal-Zuluaga <jeaz.git@gmail.com>
2020-11-27 14:01:13 -0500

    feature: made /examples/optimizer_oscillator.py easily reusable

--- examples/optimizer_oscillator.py
+++ examples/optimizer_oscillator.py
@@ -14,7 +14,7 @@ from cmodel import  seirv_model as seirv
 from cmodel import optimizer
 
 
-class Oscillator(seirv.CompartmentalModel):
+class ModelOscillator(seirv.CompartmentalModel):
     def build_model(self, t, y, w):
         """Harmonic Oscillator differential equations
         """
@@ -29,26 +29,33 @@ class Oscillator(seirv.CompartmentalModel):
         return dydt
 
 
-if __name__ == '__main__':
+def oscillator_optimizer_example(
+    q0=1.0,
+    p0=0.0,
+    w0=2 * np.pi,
+    w_bounds=[4, 8],
+    t_span=[0, 1],
+    t_steps=50,
+    fake_data_noise_factor=0.30,
+    minimization_algorithm='differential_evolution',
+    plot_solution=False
+):
 
     # Define State Variables
     q = seirv.StateVariable(
-        name='position', representation='q', initial_value=1.0
+        name='position', representation='q', initial_value=q0
     )
     p = seirv.StateVariable(
-        name='momentum', representation='p', initial_value=0.0
+        name='momentum', representation='p', initial_value=p0
     )
 
     # Define Paramters
     omega = seirv.Parameter(
-        name='frequency', representation='w', initial_value=5, bounds=[4, 8]
+        name='frequency', representation='w', initial_value=w0, bounds=w_bounds
     )
 
-    t_span = [0, 2 * np.pi / omega.initial_value]
-    t_steps = 50
-
     # Instantiate Model
-    oscillator_model = Oscillator(
+    oscillator_model = ModelOscillator(
         state_variables=[q, p],
         parameters=[omega],
         t_span=t_span,
@@ -61,9 +68,10 @@ if __name__ == '__main__':
     oscillator_solution = oscillator_model.run_model()
 
     # Build fake observation data from the solution (to test optimizer)
-    noise_factor = 0.30
-    oscillator_fake_position_data = \
-        oscillator_solution.y[0] + (2 * np.random.random(t_steps) - 1) * noise_factor
+    oscillator_fake_position_data = (
+        oscillator_solution.y[0] 
+        + (2 * np.random.random(t_steps) - 1) * fake_data_noise_factor
+        )
 
     # Optimizer using fake noisy data
     oscillator_optimizer = optimizer.Optimizer(
@@ -74,45 +82,60 @@ if __name__ == '__main__':
     )
 
     # Optimize parameters
-    minimization_algorithm = 'differential_evolution'
-    parameters_optimization = \
+    oscillator_parameters_optimization = \
         oscillator_optimizer.minimize_global(algorithm=minimization_algorithm)
 
     # Calculate differential equation solution using optimized parameters
-    if parameters_optimization.success:
-        oscillator_optimal_solution = \
-            oscillator_model.run_model(parameters=parameters_optimization.x)
+    if oscillator_parameters_optimization.success:
+        oscillator_optimal_solution = oscillator_model.run_model(
+            parameters=oscillator_parameters_optimization.x
+        )
     else:
         oscillator_optimal_solution = None
         print("Parameter optimization did not succed.")
 
-
     # Plot solution
-    plt.figure()
-    plt.plot(
-        oscillator_solution.t, oscillator_solution.y[0],
-        'k-',
-        label='Exact Solution using '
-              f'$\omega={omega.initial_value:.3f}$'
-    )
-    plt.plot(
-        oscillator_solution.t, oscillator_fake_position_data,
-        'ro', label='Noisy fake data'
-    )
-    try:
+    if plot_solution:        
+        plt.figure()
         plt.plot(
-            oscillator_optimal_solution.t, oscillator_optimal_solution.y[0],
-            'k-*',
-            label='Optimized solution from noisy data\n'
-                  f'using {minimization_algorithm} algorithm\n'
-                  f'$\omega={parameters_optimization.x[0]:.3f}$'
+            oscillator_solution.t, oscillator_solution.y[0],
+            'k-',
+            label='Exact Solution using '
+                f'$\omega={omega.initial_value:.3f}$'
         )
-    except:
-        pass
-    plt.xlabel('t')
-    plt.ylabel('q(t)')
-    plt.legend()
-    plt.grid()
-    plt.tight_layout()
-    plt.show()
-    plt.close()
+        plt.plot(
+            oscillator_solution.t, oscillator_fake_position_data,
+            'ro', label='Noisy fake data'
+        )
+        try:
+            plt.plot(
+                oscillator_optimal_solution.t, oscillator_optimal_solution.y[0],
+                'k-*',
+                label='Optimized solution from noisy data\n'
+                    f'using {minimization_algorithm} algorithm\n'
+                    f'$\omega={oscillator_parameters_optimization.x[0]:.3f}$'
+            )
+        except:
+            pass
+        plt.xlabel('t')
+        plt.ylabel('q(t)')
+        plt.legend()
+        plt.grid()
+        plt.tight_layout()
+        plt.show()
+        plt.close()
+
+    oscillator_optimization_example = {
+        'model': oscillator_model,
+        'solution': oscillator_solution,
+        'fake_position_data': oscillator_fake_position_data,
+        'optimizer': oscillator_optimizer,
+        'parameters_optimization': oscillator_parameters_optimization,
+        'optimal_solution': oscillator_optimal_solution,
+    }
+
+    return oscillator_optimization_example
+
+
+if __name__ == '__main__':
+    oscillator_optimizer_example(plot_solution=True)
