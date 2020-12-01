e980a94f856a00709cbe59c17dafc48084e4d56f
Juan Aristizabal-Zuluaga <jeaz.git@gmail.com>
2020-12-01 17:02:42 -0500

    feature: added simple setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cdslib_cmodel',
+    version='0.0.0',
+    description='Contagious Disease Simulation - Compartmental Models',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Juan Esteban Aristizabal Zuluaga',
+    author_email='jeaz.git@gmail.com',
+    license='gplv3',
+    packages=setuptools.find_packages(),
+    zip_safe=False
+    )
